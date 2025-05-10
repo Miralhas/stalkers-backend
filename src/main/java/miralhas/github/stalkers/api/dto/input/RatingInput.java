e1af9c407db49082df5f8995054f5969c0288642
Miralhas <victor2.miralhas@gmail.com>
2025-05-10 06:53:11 -0300

    feat: rating input validation

---
+++ src/main/java/miralhas/github/stalkers/api/dto/input/RatingInput.java
@@ -0,0 +1,16 @@
+package miralhas.github.stalkers.api.dto.input;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import miralhas.github.stalkers.config.validation.RatingRange;
+
+public record RatingInput(
+
+		@Min(1)
+		@Max(5)
+		@NotNull
+		@RatingRange
+		Double ratingValue
+) {
+}
