ad41f7f4a93df51eda54038d5d79c7db2b599515
Miralhas <victor2.miralhas@gmail.com>
2025-09-22 19:19:37 -0300

    feat: add min size validation on BulkChaptersInput

--- src/main/java/miralhas/github/stalkers/api/dto/input/BulkChaptersInput.java
+++ src/main/java/miralhas/github/stalkers/api/dto/input/BulkChaptersInput.java
@@ -1,11 +1,13 @@
 package miralhas.github.stalkers.api.dto.input;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record BulkChaptersInput(
 		@Valid
+		@Size(min = 1)
 		List<ChapterInput> chapters
 ) {
 }
