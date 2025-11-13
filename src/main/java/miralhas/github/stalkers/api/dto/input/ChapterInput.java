ed7e8428722dc6a96bf7b904441a67106831af41
Miralhas <victor2.miralhas@gmail.com>
2025-11-12 22:24:11 -0300

    feat: add max size to title on chapterinput

--- src/main/java/miralhas/github/stalkers/api/dto/input/ChapterInput.java
+++ src/main/java/miralhas/github/stalkers/api/dto/input/ChapterInput.java
@@ -2,9 +2,11 @@ package miralhas.github.stalkers.api.dto.input;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record ChapterInput(
 		@NotBlank
+		@Size(max = 250)
 		String title,
 
 		@NotBlank
